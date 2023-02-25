08bd7ea91428a8bff1385ee0b292934595438706
ColettoG <124051911+ColettoG@users.noreply.github.com>
2023-02-24 21:42:37 -0300

    Finalização Sprint 2
    Colocando o GDD e atualizando algumas sprites

--- src/3semana/config.gd
+++ src/3semana/config.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-onready var voltar = $config/Fundo/voltar
 
 func _on_voltar_pressed():
-	get_tree().change_scene("res://inicio.tscn")
+	get_tree().change_scene("res://Inicio.tscn")
 	# volta para a tela inicial
